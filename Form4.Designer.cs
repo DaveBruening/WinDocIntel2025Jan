34a121247d4d577ab3aa82f9d09880e71d548fda
DaveBruening <76656332+DaveBruening@users.noreply.github.com>
2025-01-27 16:24:08 -0500

    Add files via upload

---
+++ Form4.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace WinDocIntel2025Jan {
+    partial class Form4 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            comboBox1 = new ComboBox();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.AutoScroll = true;
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(673, 563);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(0, 0);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(700, 600);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Paint += pictureBox1_Paint;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(721, 63);
+            button2.Name = "button2";
+            button2.Size = new Size(274, 33);
+            button2.TabIndex = 2;
+            button2.Text = "detect words";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(721, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(182, 33);
+            comboBox1.TabIndex = 3;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(721, 118);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(371, 445);
+            textBox1.TabIndex = 1;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1125, 594);
+            Controls.Add(textBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(button2);
+            Controls.Add(panel1);
+            Name = "Form4";
+            Text = "Form4";
+            Load += Form4_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+    }
+}
---
+++ Form4.cs
@@ -0,0 +1,86 @@
+﻿using Azure;
+using Azure.AI.FormRecognizer.DocumentAnalysis;
+using System.Data;
+namespace WinDocIntel2025Jan {
+    public partial class Form4 : Form {
+        public Form4() {
+            InitializeComponent();
+        }
+        AzureKeyCredential akc;
+        DocumentAnalysisClient dac;
+        AnalyzeResult ar;
+        bool picBoxNull = true;
+        string nl = Environment.NewLine;
+        private void Form4_Load(object sender, EventArgs e) {
+            string leKey = Environment.GetEnvironmentVariable("formRecog_key");
+            string endPnt = Environment.GetEnvironmentVariable("formRecog_url");
+            akc = new AzureKeyCredential(leKey);
+            dac = new DocumentAnalysisClient(new Uri(endPnt), akc);
+            //pictureBox1.Image = Image.FromFile("layout.png"); //SecMicrosoft.jpg works
+            panel1.Controls.Add(pictureBox1);
+            comboBox1.Items.Add("read.png");
+            comboBox1.Items.Add("insurance-card.png");
+            comboBox1.Items.Add("w2.png");
+            comboBox1.Items.Add("receipt.png");
+            comboBox1.Items.Add("business_card.jpg");
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            //textBox1.Text = comboBox1.SelectedItem.ToString();
+            /* Image URIs and model IDs from: 
+             https://learn.microsoft.com/en-us/azure/ai-services/document-intelligence/
+             quickstarts/get-started-sdks-rest-api?view=doc-intel-3.0.0&preserve-view=
+             true&pivots=programming-language-rest-api */
+            string strModel ="";
+            switch (comboBox1.SelectedItem.ToString()) {
+                case "read.png": strModel= "prebuilt-read"; break;
+                case "insurance-card.png": 
+                    strModel= "prebuilt-healthInsuranceCard.us"; break;
+                case "w2.png": strModel = "prebuilt-tax.us.w2"; break;
+                case "receipt.png": strModel = "prebuilt-receipt"; break;
+                case "business_card.jpg": strModel= "prebuilt-businessCard"; break;
+                default: strModel = "WTF!?"; break;
+            }
+            string strUri = "https://raw.githubusercontent.com/Azure-Samples/" +
+                "cognitive-services-REST-api-samples/master/curl/form-recognizer/"
+                + "rest-api/" + comboBox1.SelectedItem.ToString();
+            pictureBox1.Load(strUri);
+            AnalyzeDocumentOperation ado =
+                dac.AnalyzeDocumentFromUri(WaitUntil.Completed, strModel, new Uri(strUri));
+            ar = ado.Value;
+        }
+        private void button2_Click(object sender, EventArgs e) {
+            IEnumerable<DocumentWord> idw;
+            textBox1.Text = "Like, the # of words, or something: " +
+                $"{ar.Pages[0].Words.Count}{nl}";
+            string ltr = "";
+            for (int ß=26; ß>0; ß--) {
+                ltr = System.Text.Encoding.ASCII.GetString(new byte[] { System.Convert.ToByte(64 + ß) });
+                idw = ar.Pages[0].Words.Where(w => w.Content.ToUpper().StartsWith(ltr));
+                textBox1.Text += $"# words that start with {ltr}: {idw.Count()}{nl}";
+                foreach (DocumentWord dw in idw) {
+                    textBox1.Text += String.Format("\t{0} - {1}{2}", dw.Content, dw.Confidence, nl);
+                }
+            }
+            picBoxNull = false;
+            pictureBox1.Refresh();
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e) {
+            if (!picBoxNull) {
+                Pen sive = new Pen(Color.Purple);
+                sive.Width = 3;
+                int cnt=0;
+                
+                foreach (DocumentWord dw in ar.Pages[0].Words) {
+                    PointF[] pfa = dw.BoundingPolygon.ToArray();
+                    switch (cnt++ % 4) {
+                        case 0: sive.Color= Color.Red; break;
+                        case 1: sive.Color= Color.Green; break;
+                        case 2: sive.Color= Color.Blue; break;
+                        default: sive.Color= Color.Purple; break;
+                    }
+                    e.Graphics.DrawPolygon(sive, pfa);
+                }
+            }
+        }
+    }
+}
